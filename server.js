c3725e927832789e4a265848726a537ce20d67b8
Kostas9999 <b00148740@mytudublin.ie>
2023-03-07 22:20:18 +0000

    fcv

--- server.js
+++ server.js
@@ -32,15 +32,15 @@ let client;
 let my_UID;
 module.exports = my_UID;
 let checkPassive_Interval = 1800000;
-let checkMid_Interval = 11000;
-let checkActive_Interval = 10000;
-let checkUserId_Interval = 20000;
+let checkMid_Interval = 6000;
+let checkActive_Interval = 5000;
+let checkUserId_Interval = 10000;
 
 // ===========================================  connection settings
 const options = {
-   host: "185.38.61.93",
+  // host: "185.38.61.93",
   //servername: "localhost",
- // host: "127.0.0.1",
+  host: "127.0.0.1",
   port: 57070,
 
   //ca: fs.readFileSync("./cert/ca.pem"),
@@ -103,13 +103,13 @@ function onERORR(e) {
 //============================================================
 //====================== Send Data ===========================
 
-async function sendData() {
+function sendData() {
   active_Interval = setInterval(sendActiveData, checkActive_Interval);
   mid_Interval = setInterval(sendMidData, checkMid_Interval);
   passive_Interval = setInterval(sendPassiveData, checkPassive_Interval);
 }
 
-async function sendActiveData() {
+function sendActiveData() {
   getActiveData().then((data) => {
     buffer.setActive(data);
     client.write(
@@ -122,9 +122,9 @@ async function sendActiveData() {
   });
 }
 
-async function sendMidData() {
+function sendMidData() {
   getNetwork();
-   getMidData().then((data) => {
+  getMidData().then((data) => {
     buffer.setMid(data);
     client.write(
       JSON.stringify({
@@ -136,7 +136,7 @@ async function sendMidData() {
   });
 }
 
-async function sendPassiveData() {
+function sendPassiveData() {
   getPassivedata().then((data) => {
     buffer.setPassive(data);
     client.write(
@@ -171,4 +171,20 @@ Arp.getArp().then((data) => {
 });
 */
 
+/*
+Traffic: Monitor the amount of data being transferred This can help identify potential bottlenecks or connectivity issues.
 
+Bandwidth: Monitor the available bandwidth  - network is becoming overloaded.?
+
+Latency: Monitor the amount of time it takes for data to travel across the network, also known as "ping" time. High latency can indicate a problem with the network or a specific connection.
+
+Packet loss: - This can be an indication of a problem with the network or a specific device.
+
+Availability: Monitor the availability of network devices and services. This can include devices like routers, switches, and servers, as well as services like DNS or DHCP.
+
+Security: Monitor network security-related events, such as attempted unauthorized access or suspicious traffic patterns.
+
+CPU, Memory, Disk usage: Monitor the usage - a device is overloaded and need to be replaced or upgraded.
+
+
+*/
